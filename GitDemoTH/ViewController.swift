41723c39ed747823297ac7add9699d8e2ad5253f
Marc Jones <marcjones@marcs-macbook-pro.local>
2016-07-12 13:22:44 +0100

    Initial Commit

---
+++ GitDemoTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitDemoTH
+//
+//  Created by Marc Jones on 12/07/2016.
+//  Copyright © 2016 Marc Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
